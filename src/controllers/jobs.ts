114e6d463617439aee955380a4cbfea0021c90d1
Ben GREGORY <bengregorygbr@gmail.com>
2024-02-23 00:27:10 +0100

    feat : added jobs fetching and updating

---
+++ src/controllers/jobs.ts
@@ -0,0 +1,21 @@
+import express from "express";
+import type { Request, Response } from "express";
+import { jobManagerInstance } from "../services/jobs";
+
+const router = express.Router()
+const pappers_api_key = process.env.PAPPERS_API_KEY
+const pappers_api_url = process.env.PAPPERS_API_URL || ".ENV MISSING"
+
+
+
+/**
+ * @returns all the jobs that are in progress.
+ */
+router.get('/jobs', (req:Request, res:Response)=>{
+    // Idée : récupérer tout les job enregistrés dans webhook.site
+    const jobsInProgress = jobManagerInstance.getJobs()
+    res.send(jobsInProgress)
+})
+
+
+export {router}
---
+++ src/services/jobs.ts
@@ -0,0 +1,69 @@
+import { log } from "../utils/logger";
+
+interface JobManager {
+    jobs: string[]
+}
+
+class JobManager {
+    constructor() {
+        this.jobs = [];
+    }
+
+    addJob(jobId: string) {
+        this.jobs.push(jobId);
+    }
+
+    removeJob(jobId: string) {
+        this.jobs = this.jobs.filter((job) => job !== jobId);
+    }
+
+    getJobs() {
+        return this.jobs;
+    }
+}
+
+// Create a singleton instance
+const jobManagerInstance = new JobManager();
+
+const sendToWebhook = async (companies: Set<object>, jobId: string) => {
+    const companiesArray = Array.from(companies)
+    const url = process.env.WEBHOOK_URL || null
+
+    try {
+        if (url === null) throw new Error("Missing WebHook URL")
+
+
+        jobManagerInstance.addJob(jobId)
+
+
+        fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(companiesArray),
+        })
+            .then((res) => {
+                // TODO: REMOVE DELAY
+                setTimeout(() => {
+                    // request is finished remove job 
+                    jobManagerInstance.removeJob(jobId)
+                },10000)
+            })
+    }
+    catch (err: any) {
+        log(err)
+    }
+
+}
+
+
+
+
+
+
+
+export {
+    sendToWebhook,
+    jobManagerInstance
+}
