5837e4948ade82df4fa488efb04175f3fc92aa53
Ben GREGORY <bengregorygbr@gmail.com>
2024-02-26 14:16:35 +0100

    feat : returning html instead of string on '/'

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
 import express from 'express';
-import {logMiddleware} from "./src/utils/logger"
-import {router as companiesRouter} from "./src/controllers/companies"
-import {router as jobsRouter} from "./src/controllers/jobs"
-import type { Request,Response } from 'express';
+import { logMiddleware } from "./src/utils/logger"
+import { router as companiesRouter } from "./src/controllers/companies"
+import { router as jobsRouter } from "./src/controllers/jobs"
+import type { Request, Response } from 'express';
 
 
 const app = express();
@@ -12,14 +12,26 @@ const port = process.env.PORT
  * Auto logging of requests
  */
 app.use(logMiddleware)
+
 app.use(companiesRouter)
 app.use(jobsRouter)
 
 
 
-app.get("/",(req:Request, res:Response)=>{
-    
-    res.send("Hello World!")
+app.get("/", (req: Request, res: Response) => {
+
+
+    const html = `<html>
+    <body>
+        <h1>Hello Kacy!</h1>
+        <p>You can use the following routes:</p>
+        <ul>
+            <li>/company?siren=[replace by SIREN number]</li>
+            <li><a href="/jobs">/jobs</a></li>
+        </ul>
+    </body>
+    </html>`
+    res.send(html)
 })
 
 
