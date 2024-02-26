b03064b61dc65afcc28415865b8323e67236702c
Ben GREGORY <bengregorygbr@gmail.com>
2024-02-26 14:16:11 +0100

    docs : adding comments

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,5 +1,10 @@
 import type { Request, Response } from "express"
 
+/**
+ * Logs data into the console
+ * @param message String of the content to be logged
+ * @param debug Boolean to use a debug indication in the console.
+ */
 const log = (message:string, debug:boolean = false) => {
     const logMessage = `[${new Date().toISOString()}] ${message}`
     
@@ -11,6 +16,12 @@ const log = (message:string, debug:boolean = false) => {
     console.log(logMessage)
 }
 
+/**
+ * Logs all requests made to the server
+ * @param req Express Request object containing all informations
+ * @param res Express Response object
+ * @param next Function to go to the next node of the server
+ */
 const logMiddleware = (req:Request, res:Response,next:any) => {
     const logMessage = `[${new Date().toISOString()}] ${req.url}`
 
