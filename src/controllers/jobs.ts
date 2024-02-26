ce55e9a449d000bc11260ef536674f747d9b4e24
Ben GREGORY <bengregorygbr@gmail.com>
2024-02-26 14:17:11 +0100

    chore : removed imports

--- src/controllers/jobs.ts
+++ src/controllers/jobs.ts
@@ -3,9 +3,6 @@ import type { Request, Response } from "express";
 import { jobManagerInstance } from "../services/jobs";
 
 const router = express.Router()
-const pappers_api_key = process.env.PAPPERS_API_KEY
-const pappers_api_url = process.env.PAPPERS_API_URL || ".ENV MISSING"
-
 
 
 /**
